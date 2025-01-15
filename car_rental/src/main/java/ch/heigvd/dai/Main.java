323d19776f57a15ca065e15ec1a839f926f7bb4c
Adam Kurt Gruber <adam.gruber@heig-vd.ch>
2025-01-15 19:08:32 +0100

    route create users finished

---
+++ car_rental/src/main/java/ch/heigvd/dai/Main.java
@@ -0,0 +1,22 @@
+package ch.heigvd.dai;
+
+import io.javalin.Javalin;
+import ch.heigvd.dai.users.*;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Main {
+    public static final int PORT = 8080;
+
+    public static void main(String[] args) {
+        Javalin app = Javalin.create();
+
+        ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+
+        UsersController usersController = new UsersController(users);
+
+        app.post("/users", usersController::create);
+
+        app.start(PORT);
+    }
+}
